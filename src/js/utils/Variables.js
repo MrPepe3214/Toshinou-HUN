21d0e24b12226b0211ee0ec8ca782ca6d9b7dbaf
MrPepe <43724058+MrPepe3214@users.noreply.github.com>
2018-10-12 16:30:27 +0200

    Add files via upload

---
+++ src/js/utils/Variables.js
@@ -0,0 +1,106 @@
+class Variables {
+  static get boxType() {
+    return "_-841"; 
+  }
+
+  static get attackerId() {
+    return "_-k3H"; 
+  }
+
+  static get attackHp() {
+    return "_-T22"; 
+  }
+
+  static get attackShd() {
+    return "_-g3N"; 
+  }
+
+  static get attackedId() {
+    return "_-Po"; 
+  }
+
+  static get heroAttackedId() {
+    return "_-A3x"; 
+  }
+
+  static get moveDuration() {
+    return "_-m2U"; 
+  }
+
+  static get shipDestoyedId() {
+    return "_-Y3F"; 
+  }
+
+  static get heroInitMaxHp() {
+    return "_-we"; 
+  }
+
+  static get heroInitMaxShd() {
+    return "_-u33"; 
+  }
+
+  static get heroInitHp() {
+    return "_-d0"; 
+  }
+  
+  static get heroUpdateShd(){
+    return "_-fh"; 
+  }
+
+  static get hpUpdateMaxHp() {
+    return "_-f3r"; 
+  }
+
+  static get hpUpdateHp() {
+    return "_-611"; 
+  }
+
+  static get selectMaxHp() {
+    return "_-f3r"; 
+  }
+
+  static get selectMaxShd() {
+    return "_-u33"; 
+  }
+
+  static get selectHp() {
+    return "_-611"; 
+  }
+
+  static get clanDiplomacy() {
+    return "_-G1n"; 
+  }
+
+  static get gateId() {
+    return "_-Jh"; 
+  }
+
+  static get gateType() {
+    return "_-q23"; 
+  }
+
+  static get resource() {
+    return "_-P2D"; 
+  }
+
+  static get resourceType() {
+    return "_-k3s"; 
+  }
+
+  static get assetCreateX() {
+    return "_-85y"; 
+  }
+
+  static get assetCreateY() {
+    return "_-71I"; 
+  }
+
+  static get battlestationClanDiplomacy() {
+    return "_-r3o"; 
+  }
+
+  static get heroPetId() {
+    return "_-81A"; 
+  }
+  
+}
